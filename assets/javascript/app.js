ab96f4bb9f231172d5e81a9c380676c37f555fa2
Lappy Mc Lap Face <ephillipsiii>
2018-03-05 19:07:50 -0700

    some changes before class

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,33 @@
 $(document).ready(function(){
-    $('#starButton').on("click", function(){
-        
+    $('#startButton').on("click", function(){
+//remove start button on click
+
+//populate question div with a random question from the pool oin html
+
+//populate each answer button with an answer from the the questions pool in html
+
+//start timer for each question
+//update html with timer
+
+//recognize user click
+
+//if correct, update score
+//if correct, show correct answer and maybe gif
+//if correct, reset timer
+//if correct, chose new question and update html and buttons
+
+
+//if incorrect, update score
+//if incorrect, show correct answer and maybe loosing gif
+//if incorrect reset timer
+//if incorrect, chose new question and update html and buttons
+
+//if no answer chosen, update score (unanswered)
+//if timer = 0, update score(unanswered)
+//after all questions run through, update final scores
+//present win/lose gif
+//present start over button
+//start over button resets the game
     })
 
 
